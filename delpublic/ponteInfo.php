9865d4540067b28d4fc79a4bf57b6156001816f5
Guilherm3Rodrigues <guilherme.rodrigues2@live.com>
2023-11-26 15:48:55 -0300

    testando include

--- delpublic/ponteInfo.php
+++ delpublic/ponteInfo.php
@@ -1,39 +1,7 @@
 <?php 
-//Segurança 2º caso require fails
 
+    include("../private_delivery/dados.php");
 
-/*if ($_SERVER["REQUEST_METHOD"] == "POST") 
-
-{
-
-        $categoria = $_POST['categoria'];
-        $produto = $_POST['produto'];
-        $valor = $_POST['valor'];
-
-        if (empty($categoria) || empty($produto) || empty($valor)) 
-        {
-            if (isset($_GET['acao']) && $_GET['acao'] == 'removerEdit') 
-            {
-                require_once "../private_delivery/dados.php";
-            
-            } 
-            else
-            {
-                header('location: index.php?erro=1');
-            } 
-            
-        } 
-        else 
-        {
-            require_once "../private_delivery/dados.php";
-        }
-}  
-    
-else 
-{
-*/
-    require_once "../private_delivery/dados.php";
-//}
 
 
 
